4f1ad388355ed0a69f58c4c753bc26d935ee8d7c
dmoon <dmoon.t@gmail.com>
2016-05-18 14:36:05 +0800

    use scrollview replace list view

--- app/containers/NearbyPostList.js
+++ app/containers/NearbyPostList.js
@@ -2,8 +2,9 @@ import React, {
   View,
   Component,
   ListView,
-  Alert,
-  Linking,
+  ScrollView,
+  Image,
+  Text,
 } from 'react-native';
 import SwipeOut from 'react-native-swipeout';
 import { connect } from 'react-redux';
@@ -13,6 +14,7 @@ import { requestPathData } from '../actions/PathDataActions';
 import { checkIsFav, requestAddFavorite, requestRemoveFavorite } from '../actions/FavoriteActions';
 import { calcDistance } from '../utils/place';
 const StyleSheet = require('../utils/F8StyleSheet');
+const picNoFavItem = require('../images/no-fav-item.png');
 const styles = StyleSheet.create({
   content: {
     flex: 1,
@@ -25,18 +27,37 @@ const styles = StyleSheet.create({
       marginTop: 55,
     },
   },
+  picNoFavItem: {
+    alignSelf: 'auto',
+    // alignItems: 'stretch',
+    width: 250,
+    height: 177,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textNoFavItem: {
+    marginTop: 50,
+    color: '#666',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  picContainer: {
+    paddingTop: 100,
+    flex: 0.8,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
 });
 
 
 export default class PostList extends Component {
   constructor(props) {
     super(props);
-    this.getListItem = this.getListItem.bind(this);
-    const dataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
-      dataSource,
       lat: 0,
       lon: 0,
+      nearbyData: null,
     };
   }
 
@@ -45,11 +66,11 @@ export default class PostList extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props.pathList !== nextProps.pathList) {
+    if (this.props.pathList !== nextProps.pathList) {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
-            let nearbyData = [];
+            const nearbyData = [];
             nextProps.pathList.forEach((post) => {
               const distance = calcDistance(
                 post.lat,
@@ -68,7 +89,7 @@ export default class PostList extends Component {
               return parseFloat(a.distance) - parseFloat(b.distance);
             });
             this.setState({
-              dataSource: this.state.dataSource.cloneWithRows(nearbyData),
+              nearbyData,
               lat: position.coords.latitude,
               lon: position.coords.longitude,
             });
@@ -97,78 +118,88 @@ export default class PostList extends Component {
     // ]);
   }
 
-  getListItem(rowData, sectionID, rowID) {
-    let bakColor = {};
-    if (rowID % 2 === 0) {
-      bakColor = { backgroundColor: 'rgb(255, 255, 255)' };
-    } else {
-      bakColor = { backgroundColor: 'rgb(246, 246, 246)' };
-    }
-    let tagColor;
-    switch (rowData.status) {
-      case '全線封閉':
-        tagColor = 'rgb(213, 64, 64)';
-        break;
-      case '部分封閉':
-        tagColor = 'rgb(221, 105, 49)';
-        break;
-      case '注意':
-        tagColor = 'rgb(152, 221, 84)';
-        break;
-      default:
-        tagColor = 'rgba(0,0,0,0)';
-        break;
-    }
-    const swipeoutBtns = [];
-    if (!rowData.isFav) {
-      swipeoutBtns.push(
-        {
-          text: '收藏',
-          backgroundColor: 'rgb(152, 221, 84)',
-          onPress: this.props.requestAddFavorite.bind(this, rowData.id),
-        },
-      );
-    } else {
-      swipeoutBtns.push(
-        {
-          text: '取消收藏',
-          backgroundColor: 'rgb(231, 48, 43)',
-          onPress: this.props.requestRemoveFavorite.bind(this, rowData.id),
-        },
-      );
-    }
-
-    return (
-      <SwipeOut right={swipeoutBtns} autoClose >
-        <ListItem
-          id={rowData.id}
-          index={rowData.index}
-          title={rowData.title}
-          img={rowData.pic}
-          place={rowData.place}
-          status={rowData.status}
-          tagColor={tagColor}
-          level={rowData.level}
-          detail_02={rowData.detail_02}
-          description={null}
-          onItemPress={this.onListItemPress.bind(this, rowData)}
-          distance={rowData.distance}
-          bakColor={bakColor}
-          rightText={''}
-        />
-      </SwipeOut>
-    );
-  }
-
   render() {
-    return (
-      <View style={styles.content}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this.getListItem}
-          enableEmptySections
+    const ListItemArray = [];
+    let rowIndex = 0;
+    if (!!this.state.nearbyData) {
+      for (const rowData of this.state.nearbyData) {
+        let bakColor = {};
+        if (rowIndex % 2 === 0) {
+          bakColor = { backgroundColor: 'rgb(255, 255, 255)' };
+        } else {
+          bakColor = { backgroundColor: 'rgb(246, 246, 246)' };
+        }
+        rowIndex += 1;
+
+        let tagColor;
+        switch (rowData.status) {
+          case '全線封閉':
+          tagColor = 'rgb(213, 64, 64)';
+          break;
+          case '部分封閉':
+          tagColor = 'rgb(221, 105, 49)';
+          break;
+          case '注意':
+          tagColor = 'rgb(152, 221, 84)';
+          break;
+          default:
+          tagColor = 'rgba(0,0,0,0)';
+          break;
+        }
+
+        const swipeoutBtns = [
+          {
+            text: '取消收藏',
+            backgroundColor: 'rgb(231, 48, 43)',
+            onPress: this.props.requestRemoveFavorite.bind(this, rowData.id),
+          },
+        ];
+
+        ListItemArray.push(
+          <SwipeOut right={swipeoutBtns} autoClose key={rowData.id}>
+            <ListItem
+              id={rowData.id}
+              index={rowData.index}
+              title={rowData.title}
+              img={rowData.pic}
+              place={rowData.place}
+              status={rowData.status}
+              tagColor={tagColor}
+              level={rowData.level}
+              detail_02={rowData.detail_02}
+              description={null}
+              onItemPress={this.onListItemPress.bind(this, rowData)}
+              distance={rowData.distance}
+              bakColor={bakColor}
+              rightText={''}
+              />
+          </SwipeOut>
+        );
+      }
+    }
+
+    let contentChildren = null;
+    if (this.state.nearbyData === null) {
+      contentChildren = <View />;
+    } else if (ListItemArray.length > 0) {
+      contentChildren = ListItemArray;
+    } else {
+      contentChildren = (
+       <View style={styles.picContainer}>
+        <Image
+          source={picNoFavItem}
+          style={styles.picNoFavItem}
         />
-    </View>
+        <Text style={styles.textNoFavItem}>
+          目前您附近沒有任何步道 :p
+        </Text>
+      </View>
+    );}
+
+    return (
+      <ScrollView style={styles.content}>
+        { contentChildren }
+      </ScrollView>
     );
   }
 }
