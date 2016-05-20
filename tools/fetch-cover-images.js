5006771a194ae3b465513881ad992a1901b6d2ce
lyhcode <lyhcode@gmail.com>
2016-05-21 02:45:18 +0800

    add tiny size image

--- tools/fetch-cover-images.js
+++ tools/fetch-cover-images.js
@@ -1,6 +1,7 @@
-var pathListData = require('../app/json/data');
+const fs = require('fs');
+const pathListData = require('../app/json/data');
 // console.log(pathListData);
-var execSync = require('child_process').execSync;
+const execSync = require('child_process').execSync;
 const coverImagesPath =  './app/images/cover';
 
 var shell = function(cmd) {
@@ -14,15 +15,19 @@ shell(`mkdir -p ${coverImagesPath}`);
 for (var path of pathListData) {
 
   shell(`mkdir -p ${coverImagesPath}/${path.id}`);
-
-  var header = '--header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11"';
-
-  console.log(`Download ${path.cover}`);
-
-  shell(`wget '${path.cover}' -O ${coverImagesPath}/${path.id}/${path.id}.jpg ${header}`);
-
+  
   var coverName = `${coverImagesPath}/${path.id}/${path.id}`;
-  shell(`convert ${coverName}.jpg -resize 1024x768 ${coverName}_l.jpg;`);
-  shell(`convert ${coverName}.jpg -resize 640x480  ${coverName}_m.jpg;`);
-  shell(`convert ${coverName}.jpg -resize 320x240  ${coverName}_s.jpg;`);
+
+  if (!fs.existsSync(`${coverName}.jpg`)) {
+    var header = '--header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11"';
+    console.log(`Download ${path.cover}`);
+    shell(`wget '${path.cover}' -O ${coverName}.jpg ${header}`);
+  }
+
+  console.log(`Compress ${coverName}.jpg`);
+
+  shell(`convert ${coverName}.jpg -quality 80 -resize 1024x768 ${coverName}_l.jpg;`);
+  shell(`convert ${coverName}.jpg -quality 80 -resize 640x480  ${coverName}_m.jpg;`);
+  shell(`convert ${coverName}.jpg -quality 80 -resize 320x240  ${coverName}_s.jpg;`);
+  shell(`convert ${coverName}.jpg -quality 60 -resize 240x160  ${coverName}_t.jpg;`);
 }
