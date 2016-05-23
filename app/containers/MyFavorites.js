f9368adad0a81a1e6491791ec19f01e32b93b638
dmoon <dmoon.t@gmail.com>
2016-05-23 17:11:36 +0800

    fav performance

--- app/containers/MyFavorites.js
+++ app/containers/MyFavorites.js
@@ -143,14 +143,14 @@ export default class MyFavorite extends Component {
       );
     }
 
-    function render() {
+    render = () => {
       let view = [];
       if (ListItemArray.length > 0) {
         view = (
         <ScrollView style={styles.content}>
           { ListItemArray }
         </ScrollView>);
-      } else {
+      } else if ( this.props.pathList !== 'null' ) {
         view = (
         <ScrollView style={styles.content}>
           <View style={styles.picContainer}>
@@ -163,6 +163,8 @@ export default class MyFavorite extends Component {
           </Text>
           </View>
         </ScrollView>);
+      } else {
+        view = <View />;
       }
       return view;
     }
@@ -179,7 +181,9 @@ MyFavorite.propTypes = {
   pathList: React.PropTypes.array,
 };
 
-MyFavorite.defaultProps = {};
+MyFavorite.defaultProps = {
+  pathList: 'null',
+};
 
 function _injectPropsFromStore(state) {
   return {
--- app/reducers/PathDataReducer.js
+++ app/reducers/PathDataReducer.js
@@ -2,19 +2,23 @@ import { UPDATE_PATH_LIST } from '../actions/PathDataActions';
 import { TOGGLE_FAVORITE } from '../actions/FavoriteActions';
 import { findObjById } from '../utils/immutable';
 
-export function pathList(state = [], action) {
+export function pathList(state = 'null', action) {
   switch (action.type) {
     case UPDATE_PATH_LIST:
       return [
         ...action.data,
       ];
     case TOGGLE_FAVORITE: {
-      const newList = findObjById(state, 'id', action.data, (item) => {
-        let newItem = {...item};
-        newItem.isFav = !item.isFav;
-        return newItem;
-      });
-      return newList;
+      if (state !== 'null') {
+        const newList = findObjById(state, 'id', action.data, (item) => {
+          let newItem = {...item};
+          newItem.isFav = !item.isFav;
+          return newItem;
+        });
+        return newList;
+      } else {
+        return 'null';
+      }
     }
     default:
       return state;
